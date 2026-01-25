d1ad8b1bda150cfecf1023d8242a95cff104af64
rhdbqls <kyb193539@naver.com>
2026-01-25 12:49:22 +0900

    ✨ Feature: 면접 시간 조회/선택 구현

--- reqruitment/check/admin.py
+++ reqruitment/check/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Applicant, InterviewTimeSlot, ApplicantTimeSlot
 
 # Register your models here.
+admin.site.register(Applicant)
+admin.site.register(InterviewTimeSlot)
+admin.site.register(ApplicantTimeSlot)
