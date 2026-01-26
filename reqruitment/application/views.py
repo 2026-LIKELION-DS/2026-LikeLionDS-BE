45ec61ad5ad7e53b567a58c94fad9f41e521eee2
b1nnnnid <suebenny2@naver.com>
2026-01-26 18:41:27 +0900

    feature: application 지원서 작성

--- reqruitment/application/views.py
+++ reqruitment/application/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import Application
+from .serializers import ApplicationSerializer
+
+class ApplicationCreateView(generics.CreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_class = [AllowAny]  # 누구나 접근 가능
